b0066a69f7dc35a74cc15fc0b0517213528fa0d6
Cindy L <TCXX@users.noreply.github.com>
2016-11-12 14:55:51 -0600

    Add files via upload

---
+++ Extension/Duration.java
@@ -0,0 +1,25 @@
+package myextension.solved;
+
+/**
+ * 
+ * @author Cindy Le
+ *
+ */
+
+	
+public class Duration {
+		
+	public final int startTime, endTime;
+		
+	public Duration (int staT, int endT) {
+		this.startTime = staT;
+		this.endTime   = endT;
+	}
+		
+	public String toString() {
+		return "Duration " + startTime + " - " + endTime;
+	}
+
+}
+
+
